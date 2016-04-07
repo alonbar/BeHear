a653a8e952dce0bcd44ff26ed5c208ae607d8f95
alon bar <alonlpc@gmail.com>
2016-04-07 11:22:25 +0300

    Starting project. Added Polygon that knows if a dot is in or out.

---
+++ BeHear/app/src/main/java/com/mycompany/behear/Point.java
@@ -0,0 +1,13 @@
+package com.mycompany.behear;
+
+/**
+ * Created by baralon on 06/04/2016.
+ */
+public class Point {
+    public double x;
+    public double y;
+    public Point (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ BeHear/app/src/main/java/com/mycompany/behear/Polygon.java
@@ -0,0 +1,26 @@
+package com.mycompany.behear;
+
+import java.util.ArrayList;
+
+/**
+ * Created by baralon on 06/04/2016.
+ */
+public class Polygon {
+
+    private ArrayList<Point> geometryTable;
+    public Polygon(ArrayList<Point> geometry) {
+        this.geometryTable =  new ArrayList<Point>(geometry);
+    }
+
+    public boolean isPointInPolygon(Point p) {
+        int i = 0, j = 0;
+        boolean result = false;
+        for (i = 0, j = geometryTable.size() - 1; i < geometryTable.size(); j = i++) {
+            if ((geometryTable.get(i).y > p.y) != (geometryTable.get(j).y > p.y) &&
+                    (p.x < (geometryTable.get(j).x - geometryTable.get(i).x) * (p.y - geometryTable.get(i).y) / (geometryTable.get(j).y - geometryTable.get(i).y) + geometryTable.get(i).x)) {
+                result = !result;
+            }
+        }
+        return result;
+    }
+}
